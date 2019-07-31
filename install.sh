11c0cd9b8810c4481ab59a61387a6fc5200cc643
Astashkin Alexander <astashkinav@gmail.com>
2019-07-31 09:50:48 +0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+wp core download --force --allow-root
+wp config create \
+  --dbhost=$WORDPRESS_DB_HOST \
+  --dbname=$WORDPRESS_DB_NAME \
+  --dbuser=$WORDPRESS_DB_USER \
+  --dbpass=$WORDPRESS_DB_PASSWORD \
+  --allow-root
+wp core install \
+  --title=$TITLE \
+  --url=$URL \
+  --admin_user=$ADMIN_USER \
+  --admin_password=$ADMIN_PASSWORD \
+  --admin_email=$ADMIN_EMAIL \
+  --allow-root
